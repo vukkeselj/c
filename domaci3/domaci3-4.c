b8dd93fd6d9886c032931340935762ca03d6b6bc
vukkeselj <vukkeselj@gmail.com>
2019-04-03 00:13:40 +0200

    commit

--- domaci3/domaci3-4.c
+++ domaci3/domaci3-4.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #define STR_MAX 100
 
-
-int main()
+int fib(int n)
 {
     int i = 0;
-    int n = 0;
+    
     int a[100];
     //a[2]==1;
-    printf("Unesite n: ");
-    scanf("%d", &n);
     for (i=0; i<n; i++)
     {
        a[i]=a[i-1]+a[i-2];
        printf("%d\n", a[i]);
     }
 }
+
+int main()
+{
+    int m=0;
+    printf("Unesite m: ");
+    scanf("%d", &m);
+    int y = fib(m);
+    //printf("%d\n", y)
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
